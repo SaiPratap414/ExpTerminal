199f25181e49665f74b2a29398a34f0af0e3e626
pratap@illiquidlabs.com <pratap@illiquidlabs.com>
2024-01-30 19:43:50 +0530

    navbar

--- src/pages/Terminal.js
+++ src/pages/Terminal.js
@@ -1,3 +1,4 @@
+import MultichainMemecoinCard from "../components/MultichainMemecoinCard";
 import styles from "./Terminal.module.css";
 
 const Terminal = () => {
@@ -15,73 +16,31 @@ const Terminal = () => {
                 <div className={styles.link}>Coming Soon</div>
               </div>
             </div>
-            <div className={styles.navbar2}>
-              <div className={styles.container}>
-                <div className={styles.column}>
+            <div className={styles.navbar}>
+              <div className={styles.column}>
+                <img className={styles.omnicatIcon} alt="" src="/omnicat.svg" />
+              </div>
+              <div className={styles.column1}>
+                <b className={styles.link1}>Mission</b>
+                <b className={styles.link1}>Terminal</b>
+                <b className={styles.link1}>Ecosystem</b>
+                <b className={styles.link1}>Statistics</b>
+              </div>
+              <div className={styles.column2}>
+                <div className={styles.vectorParent}>
+                  <img className={styles.vectorIcon} alt="" src="/vector.svg" />
                   <img
-                    className={styles.omnicatIcon}
+                    className={styles.vectorIcon}
                     alt=""
-                    src="/omnicat.svg"
+                    src="/subtract.svg"
                   />
-                </div>
-                <div className={styles.column1}>
-                  <b className={styles.text}>Mission</b>
-                  <b className={styles.text}>Terminal</b>
-                  <b className={styles.text}>Ecosystem</b>
-                  <b className={styles.text}>Statistics</b>
-                </div>
-                <div className={styles.column2}>
-                  <div className={styles.vectorParent}>
-                    <img
-                      className={styles.vectorIcon}
-                      alt=""
-                      src="/vector.svg"
-                    />
-                    <img
-                      className={styles.vectorIcon}
-                      alt=""
-                      src="/subtract.svg"
-                    />
-                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className={styles.footer4}>
-        <div className={styles.content}>
-          <div className={styles.logo}>
-            <img className={styles.omnicatIcon} alt="" src="/omnicat.svg" />
-            <b className={styles.link}>The First ever Multichain Memecoin</b>
-          </div>
-          <div className={styles.links}>
-            <b className={styles.text}>Link One</b>
-            <b className={styles.text}>Link Two</b>
-            <b className={styles.text}>Link Three</b>
-            <b className={styles.text}>Link Four</b>
-            <b className={styles.text}>Link Five</b>
-          </div>
-          <div className={styles.socialLinks}>
-            <div className={styles.vectorGroup}>
-              <img className={styles.vectorIcon1} alt="" src="/vector.svg" />
-              <img className={styles.vectorIcon1} alt="" src="/subtract.svg" />
-            </div>
-          </div>
-        </div>
-        <b className={styles.link}>© All rights reserved, 2024</b>
-        <div className={styles.credits}>
-          <div className={styles.divider} />
-          <div className={styles.row}>
-            <b className={styles.text}>2023 Relume. All right reserved.</b>
-            <div className={styles.row}>
-              <b className={styles.link12}>Privacy Policy</b>
-              <b className={styles.link12}>Terms of Service</b>
-              <b className={styles.link12}>Cookies Settings</b>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MultichainMemecoinCard />
     </div>
   );
 };
