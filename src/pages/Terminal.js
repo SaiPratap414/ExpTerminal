0a94e4435087f4b3ef2213d2f9402c721416e249
pratap@illiquidlabs.com <pratap@illiquidlabs.com>
2024-01-31 15:48:39 +0530

    video added

--- src/pages/Terminal.js
+++ src/pages/Terminal.js
@@ -6,35 +6,37 @@ const Terminal = () => {
     <div className={styles.terminal}>
       <div className={styles.header14Wrapper}>
         <div className={styles.header14}>
-          <div className={styles.placeholderLightbox}>
-            <div className={styles.playButton} />
-            <div className={styles.frameParent}>
-              <div className={styles.terminalWrapper}>
-                <div className={styles.terminal1}>Terminal</div>
-              </div>
-              <div className={styles.linkWrapper}>
-                <div className={styles.link}>Coming Soon</div>
-              </div>
+          <video className={styles.placeholderLightbox}  autoPlay loop muted>
+            <source src="Comp.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <div className={styles.playButton} />
+          <div className={styles.frameParent}>
+            <div className={styles.terminalWrapper}>
+              <div className={styles.terminal1}>Terminal</div>
             </div>
-            <div className={styles.navbar}>
-              <div className={styles.column}>
-                <img className={styles.omnicatIcon} alt="" src="/omnicat.svg" />
-              </div>
-              <div className={styles.column1}>
-                <b className={styles.link1}>Mission</b>
-                <b className={styles.link1}>Terminal</b>
-                <b className={styles.link1}>Ecosystem</b>
-                <b className={styles.link1}>Statistics</b>
-              </div>
-              <div className={styles.column2}>
-                <div className={styles.vectorParent}>
-                  <img className={styles.vectorIcon} alt="" src="/vector.svg" />
-                  <img
-                    className={styles.vectorIcon}
-                    alt=""
-                    src="/subtract.svg"
-                  />
-                </div>
+            <div className={styles.linkWrapper}>
+              <div className={styles.link}>Coming Soon</div>
+            </div>
+          </div>
+          <div className={styles.navbar}>
+            <div className={styles.column}>
+              <img className={styles.omnicatIcon} alt="" src="/omnicat.svg" />
+            </div>
+            <div className={styles.column1}>
+              <b className={styles.link1}>Mission</b>
+              <b className={styles.link1}>Terminal</b>
+              <b className={styles.link1}>Ecosystem</b>
+              <b className={styles.link1}>Statistics</b>
+            </div>
+            <div className={styles.column2}>
+              <div className={styles.vectorParent}>
+                <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+                <img
+                  className={styles.vectorIcon}
+                  alt=""
+                  src="/subtract.svg"
+                />
               </div>
             </div>
           </div>
